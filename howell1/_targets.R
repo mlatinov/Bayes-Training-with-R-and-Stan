388c145f67c2fac4aa8faf4caf9f4112dacc780a
mlatinov <latinovmetodi@gmail.com>
2025-10-30 23:45:38 +0300

    targets file

---
+++ howell1/_targets.R
@@ -0,0 +1,39 @@
+
+#### Libraries ####
+library(targets)
+library(tidyverse)
+library(brms)
+library(bayesplot)
+library(posterior)
+library(dagitty)
+library(ggdag)
+
+#### Source Functions ####
+source("functions/howell_make_dag.R")
+source("functions/howell_make_gen.R")
+
+#### Pipeline ####
+list(
+
+  ## Load the data ##
+  tar_target(
+    name = howell_data,
+    command = read_delim("data/Howell1.csv",
+      delim = ";",
+      escape_double = FALSE,
+      trim_ws = TRUE)
+    ),
+
+  ## Causal model DAG ##
+  tar_target(
+    name = howell_dag,
+    command = howell_make_dag(howell_data)
+    ),
+
+  ## Generative model ##
+  tar_target(
+    name = howell_gen,
+    command = howell_make_gen(10000)
+  )
+
+)
