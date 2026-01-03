3c9ddecbada5adcfbd59af961e38502720290bd4
mlatinov <latinovmetodi@gmail.com>
2026-01-03 21:56:12 +0300

    targets update

--- Memory Test on Drugged Islanders/_targets.R
+++ Memory Test on Drugged Islanders/_targets.R
@@ -60,14 +60,39 @@ list(
     seed = 123
   ),
 
-  ## Delta Score and Drug Type
-  tar_stan_mcmc(
-    name = aov_delta_drug_type,
-    stan_files = "",
-    data = list(),
-    chains = 4,
-    iter_sampling = 1000,
-    seed = 123
-  )
+  ####  Bayesian Models ####
 
+  ## Multilevel Model with var Intercepts for Memory After
+  tar_stan_mcmc(
+    name = m_1,
+    stan_files = "stan_scripts/bayes_model_1.stan",
+    data = list(
+      N = nrow(clean_data),
+      J = nlevels(clean_data$drug_type),
+      Dosage = as.numeric(as.character(clean_data$dosage)),
+      Drug_group_idx = as.numeric(as.character(clean_data$drug_type)) + 1,
+      Memory = clean_data$memory_score_after,
+      Age = clean_data$age
+    ),
+    chains = 4,
+    seed = 123,
+    iter_sampling = 1000
+    ),
+
+  ## Multilevel Model with var Intercepts for Delta
+  tar_stan_mcmc(
+    name = m_2,
+    stan_files = "stan_scripts/bayes_model_2.stan",
+    data = list(
+      N = nrow(clean_data),
+      J = nlevels(clean_data$drug_type),
+      Dosage = as.numeric(as.character(clean_data$dosage)),
+      Group_idx = as.numeric(as.character(clean_data$drug_type)) + 1,
+      Delta_score = clean_data$delta_score,
+      Age = clean_data$age
+    ),
+    chains = 4,
+    seed = 123,
+    iter_sampling = 1000
+  )
 )
